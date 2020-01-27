fa3e1ac0e73a5b357a1ad01f4f359d80c31e5f74
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-01-27 15:20:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
-# setup.py
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pybyte-katurian", 
+    version="0.0.1",
+    author="Kate Kulinski",
+    author_email="katekulinski@gmail.com",
+    description="API Wrapper for Byte",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/katurian/pybyte",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
