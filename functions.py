64229299226399cbea78cd3f8ded3b8c107f09e2
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-01-27 18:39:27 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,6 +3,11 @@ import requests
 
 authtoken = 'XXXXXXEXNFWBXXXXXXYCZRXXXX'
 
+def getFeed():
+    r = requests.get('https://api.byte.co/feed/global', headers={'Authorization': authtoken})
+    feedJSON = r.json()
+    return feedJSON
+
 def getAccountInfo():
     r = requests.get('https://api.byte.co/account/me', headers={'Authorization': authtoken})
     accountJSON = r.json()
@@ -12,11 +17,16 @@ def getBlockedList():
     r = requests.get('https://api.byte.co/account/me/blocking', headers={'Authorization': authtoken})
     blockedJSON = r.json()
     return blockedJSON
+
+def getMyPosts():
+    r = requests.get('https://api.byte.co/account/me/posts', headers={'Authorization': authtoken})
+    myPostsJSON = r.json()
+    return myPostsJSON
     
-def getFeed():
-    r = requests.get('https://api.byte.co/feed/global', headers={'Authorization': authtoken})
-    feedJSON = r.json()
-    return feedJSON
+def getUserPosts(userID):
+    r = requests.get('https://api.byte.co/account/id/{userID}/posts', headers={'Authorization': authtoken})
+    userPostsJSON = r.json()
+    return userPostsJSON
     
 def getUserInfo(userID): 
     r = requests.get(f'https://api.byte.co/account/id/{userID}', headers={'Authorization': authtoken})
@@ -24,18 +34,14 @@ def getUserInfo(userID):
     return userJSON
     
 def likePost(postID):
-    r = requests.put(f'https://api.byte.co/post/id/{postID}/feedback/like', headers={'Authorization': authtoken})
+    requests.put(f'https://api.byte.co/post/id/{postID}/feedback/like', headers={'Authorization': authtoken})
   
 def commentPost(postID):
-    r = requests.post(f'https://api.byte.co/post/id/{postID}/feedback/comment', headers={'Authorization': authtoken}, json={'body':'comment'})
+    requests.post(f'https://api.byte.co/post/id/{postID}/feedback/comment', headers={'Authorization': authtoken}, json={'body':'comment'})
   
 def blockUser(userID): 
-    r = requests.put(f'https://api.byte.co/account/id/{userID}/block', headers={'Authorization': authtoken})
+    requests.put(f'https://api.byte.co/account/id/{userID}/block', headers={'Authorization': authtoken})
     
 def followUser(userID): 
-    r = requests.put(f'https://api.byte.co/account/id/{userID}/follow', headers={'Authorization': authtoken})
+    requests.put(f'https://api.byte.co/account/id/{userID}/follow', headers={'Authorization': authtoken})
    
-    
-   
-    
-
